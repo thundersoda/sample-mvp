8941895b9a552d1c9a0c8b12adbdb387544921fd
Rodrigo Ayala <rodrigoa7902@gmail.com>
2022-07-05 09:10:14 -0400

    Added override annotation and removed unnecessary variable

--- app/src/main/java/com/thundersoda/samplemvp/stories/list/model/PokemonRepositoryImpl.java
+++ app/src/main/java/com/thundersoda/samplemvp/stories/list/model/PokemonRepositoryImpl.java
@@ -1,18 +1,15 @@
 package com.thundersoda.samplemvp.stories.list.model;
 
-import com.thundersoda.samplemvp.model.Pokemon;
 import com.thundersoda.samplemvp.model.PokemonResponse;
 import com.thundersoda.samplemvp.networking.ApiClient;
-
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class PokemonRepositoryImpl implements PokemonRepository {
-    List<Pokemon> pokemons;
 
+    @Override
     public void getListOfPokemons(OnFinishedListener onFinishedListener) {
         Call<PokemonResponse> call = ApiClient.buildApi().getPokemonList();
         call.enqueue(new Callback<PokemonResponse>() {
@@ -20,10 +17,9 @@ public class PokemonRepositoryImpl implements PokemonRepository {
             public void onResponse(Call<PokemonResponse> call, Response<PokemonResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     PokemonResponse pokemonList = response.body();
-                    pokemons = pokemonList.getData();
 
                     //Pasamos la respuesta del servicio al OnSuccessRequest
-                    onFinishedListener.onSuccess(pokemons);
+                    onFinishedListener.onSuccess(pokemonList.getData());
                 } else {
                     onFinishedListener.onFailure(response.message());
                 }
